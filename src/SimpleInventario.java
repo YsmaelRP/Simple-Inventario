4e8d0f99e4b29c0a1c71e5d31f147198af78e262
Laboratorio 109 Pc-09 <lab-109-inf-09>
2023-09-21 19:35:35 -0400

    Nueva clase con Metodos

--- src/SimpleInventario.java
+++ src/SimpleInventario.java
@@ -1,9 +1,33 @@
+import java.security.PublicKey;
 import java.util.Scanner;
 
 public class SimpleInventario {
-public static void agragarinventario(){
-    Scanner scanner = new Scanner(System.in);
+public static void agragarinventario(String s){
+    System.out.println("Agregar un Articulo: ");
+}
 
+    public static void removerArticuloint(int id) {
 }
+
+    public static double ObtenerPrecioArticulo(int id) {
+        return 0.00;
+    }
+
+        public static void imprimirinventario(){
+    System.out.println("Ingrese una opcion");
+        }
+
+        public static void main(String[] args){
+           imprimirinventario();
+        }
 }
+
+
+
+
+
+
+
+
+
 

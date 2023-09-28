908b31ae50ed200cfd34f9e65af897e11ea759a8
Laboratorio 109 Pc-09 <lab-109-inf-09>
2023-09-28 19:44:36 -0400

    mejoras aplicadas

--- src/SimpleInventario.java
+++ src/SimpleInventario.java
@@ -6,7 +6,22 @@ public static void agragarinventario(String s){
     System.out.println("Agregar un Articulo: ");
 }
 
-    public static void removerArticuloint(int id) {
+    public static String obtenerNombreArticulo(){
+      Scanner scanner = new Scanner (System.in);
+      System.out.println("Ingrese el nombre de articulo: ");
+      String nombredeArticulo;
+      nombredeArticulo = scanner.next();
+      System.out.println("Usted a ingresado el articulo" + nombredeArticulo);
+      return nombredeArticulo;
+
+    }
+
+
+
+
+
+
+        public static void removerArticuloint(int id) {
 }
 
     public static double ObtenerPrecioArticulo(int id) {
@@ -14,7 +29,13 @@ public static void agragarinventario(String s){
     }
 
         public static void imprimirinventario(){
-    System.out.println("Ingrese una opcion");
+            System.out.println("Ingrese su Nombre");
+            Scanner scanner = new Scanner(System.in);
+            String usuario;
+            usuario = scanner.next();
+            System.out.println("Bienvenido " + usuario);
+            String articuloExistente = obtenerNombreArticulo();
+            System.out.println("Articulo existente: " + articuloExistente);
         }
 
         public static void main(String[] args){
